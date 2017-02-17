3efe4d05577dcfb7df9b3012c09f63c17ac47325
Evil2l <dWill@yandex.ru>
2017-02-17 16:52:02 +0200

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -2,21 +2,27 @@
 
 const express = require('express');
 const app = express();
+const http = require('http');
 const path = require('path');
-
+const socketIo = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 5500;
+let server = http.createServer(app);
+let io = socketIo(server);
+
+
 //  server middleware
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket)=>{
+    console.log('new user connected');
+    console.log(socket);
+    socket.on('disconnect', ()=>{
+        console.log('User has just disconnected');
+    });
+});
 
-console.log(__dirname+'../public');
-console.log();
 
-// app.get('/', (res, req)=>{
-//     res.send('First check');
-// });
-
-app.listen(port, () => console.log(`March Hare ready to go on ${port}`));
+server.listen(port, () => console.log(`March Hare ready to go on ${port}`));
