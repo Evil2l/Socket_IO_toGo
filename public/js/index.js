a6c44dc2c85401587ee20caffd5c4aa9442b5435
Evil2l <dWill@yandex.ru>
2017-02-24 17:53:03 +0200

    scroll to bottom function

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,25 @@
 
 var socket =  io();
 
+//
+function scrollToBottom() {
+        // Selectors
+    var messages = $('#message-list');
+    var newMessage = messages.children('li:last-child');
+        // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+
+
+}
+
 //  listener to connect event
 socket.on('connect', function () {
     console.log('Connected to server');
@@ -29,6 +48,8 @@ socket.on('newMessage', function (message) {
 
     $('#message-list').append(html);
 
+    scrollToBottom();
+
 });
 
 socket.on('newLocationMessage', function(message){
@@ -45,14 +66,8 @@ socket.on('newLocationMessage', function(message){
 
 
     $('#message-list').append(html);
-    // var li = $('<li></li>');
-    // var a = $('<a target="_blank">My current location</a>');
-    //
-    // li.text(`${message.from} ${formattedTime}: `);
-    // a.attr('href', message.url);
-    //
-    // li.append(a);
-    // $('#message-list').append(li);
+
+    scrollToBottom();
 
 });
 
